4becec1a2fc5276b047596add685ee32d9f7561a
Ivankorytnik <koritnik2@gmail.com>
2026-07-08 11:38:37 +0300

    Update main.js

--- js|/main.js
+++ js|/main.js
@@ -1,1 +1,32 @@
 console.log("Сайт Ивана Корытника загружен");
+const galleryImages = document.querySelectorAll(".photo-gallery img");
+const photoModal = document.getElementById("photoModal");
+const photoModalImage = document.getElementById("photoModalImage");
+const photoModalClose = document.querySelector(".photo-modal__close");
+
+galleryImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    photoModalImage.src = image.src;
+    photoModalImage.alt = image.alt;
+    photoModal.classList.add("active");
+  });
+});
+
+photoModalClose.addEventListener("click", () => {
+  photoModal.classList.remove("active");
+  photoModalImage.src = "";
+});
+
+photoModal.addEventListener("click", (event) => {
+  if (event.target === photoModal) {
+    photoModal.classList.remove("active");
+    photoModalImage.src = "";
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    photoModal.classList.remove("active");
+    photoModalImage.src = "";
+  }
+});
